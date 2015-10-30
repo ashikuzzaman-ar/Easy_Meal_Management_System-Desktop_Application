7961d61794a1b25114d238ca8979842062e58b6c
ashik <ashik@ashik>
2015-10-30 20:47:47 +0600

    Change ConnectToDatabase.java for connecting with sqlite database

--- src/src/ConnectToDatabase.java
+++ src/src/ConnectToDatabase.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,37 +9,36 @@ import java.sql.Statement;
 
 public class ConnectToDatabase {
 
-    private Connection connection = null;
-    private Statement statement = null;
-    private ResultSet resultSet = null;
-    private String sql = null;
+    private static Connection connection = null;
+    private static Statement statement = null;
+    private static ResultSet resultSet = null;
+    private static String sql = null;
 
-    private void setConnection() {
+    private static void setConnection() {
 
         try {
+            
+            File file = new File("");
 
-            Class.forName("com.mysql.jdbc.Driver");
-            this.connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost/",
-                    "root",
-                    "ar840"
-            );
-            this.statement = this.connection.createStatement();
-            this.statement.execute("use omms");
-
+            Class.forName("org.sqlite.JDBC");
+            ConnectToDatabase.connection = DriverManager.getConnection(
+                    "jdbc:sqlite:"+file.getAbsolutePath()+"/.EMMS-RES/datas.sqlite");
+            ConnectToDatabase.statement = ConnectToDatabase.connection.createStatement();
+            ConnectToDatabase.statement.execute("CREATE  TABLE  IF NOT EXISTS user_info ("
+                    + "user_name VARCHAR PRIMARY KEY  NOT NULL , password VARCHAR NOT NULL )");
         } catch (ClassNotFoundException | SQLException e) {
             
             System.err.println(e);
         }
     }
 
-    private void executeQuery() {
+    private static void executeQuery() {
 
         try {
             
-            this.setConnection();
-            this.statement.execute(sql);
-            this.resultSet = this.statement.getResultSet();
+            ConnectToDatabase.setConnection();
+            ConnectToDatabase.statement.execute(sql);
+            ConnectToDatabase.resultSet = ConnectToDatabase.statement.getResultSet();
         } catch (SQLException ex) {
             
             System.err.println(ex);
@@ -46,24 +46,10 @@ public class ConnectToDatabase {
     }
     
     
-    public ResultSet getResult(String sql) {
+    public static ResultSet getResult(String sql) {
 
-        this.sql = sql;
-        this.executeQuery();
-        return this.resultSet;
-    }
-    
-    public void closeConnection() {
-        
-        try {
-            
-            this.connection.close();
-            this.statement.close();
-            this.resultSet.close();
-            this.sql = null;
-        } catch (Exception e) {
-            
-            System.err.println(e);
-        }
+        ConnectToDatabase.sql = sql;
+        ConnectToDatabase.executeQuery();
+        return ConnectToDatabase.resultSet;
     }
 }
