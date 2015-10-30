051377379ba084097cba5363b75e9d4b252386cd
ashik <ashik@ashik>
2015-10-30 20:45:47 +0600

    Change For ResourceTester.java's method

--- src/src/Main.java
+++ src/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         
         ResourceTester rs = new ResourceTester();
-        rs.check();
+        rs.createDirectory();
         MainFrame.mainFrame();
     }
 }
