c0602961ceb88896aa6ee42aa095165957e1c3c5
ashik <ashik@ashik>
2015-10-31 02:12:42 +0600

    Finalize ResourceTester.java class for sqlite database....

--- src/src/ResourceTester.java
+++ src/src/ResourceTester.java
@@ -1,4 +1,3 @@
-
 package src;
 
 import java.awt.HeadlessException;
@@ -6,32 +5,55 @@ import java.io.File;
 import javax.swing.JOptionPane;
 
 public class ResourceTester {
-        
-    private final File dummy ;
-    private final File file ;
-    
-    public ResourceTester(){
-        
+
+    private final File dummy;
+    private final File file;
+    private String sql;
+
+    public ResourceTester() {
+
         this.dummy = new File("");
-        this.file = new File(dummy.getAbsolutePath()+"/.EMMS-RES");
+        this.file = new File(dummy.getAbsolutePath() + "/EMMS-RES");
     }
-    
-    
-    
-    public void createDirectory(){
-        
-        
+
+    public void createDirectory() {
+
         try {
-            
-            if(!this.file.exists()){
-                
+
+            if (!this.file.exists()) {
+
                 this.file.mkdirs();
-                
-            }  
+
+                this.sql = "CREATE  TABLE IF NOT EXISTS member_info("
+                        + "id VARCHAR PRIMARY KEY  NOT NULL , "
+                        + "name VARCHAR NOT NULL )";
+
+                ConnectToDatabase.getResult(this.sql);
+
+                this.sql = "CREATE  TABLE  IF NOT EXISTS payment_info("
+                        + "id VARCHAR NOT NULL , "
+                        + "date VARCHAR NOT NULL , "
+                        + "amount VARCHAR NOT NULL )";
+
+                ConnectToDatabase.getResult(this.sql);
+
+                this.sql = "CREATE  TABLE  IF NOT EXISTS meal_info("
+                        + "id VARCHAR NOT NULL , "
+                        + "date VARCHAR NOT NULL , "
+                        + "meal VARCHAR)";
+
+                ConnectToDatabase.getResult(this.sql);
+
+                this.sql = "CREATE  TABLE  IF NOT EXISTS shopping_cost("
+                        + "date VARCHAR NOT NULL , "
+                        + "cost VARCHAR NOT NULL )";
+
+                ConnectToDatabase.getResult(this.sql);
+            }
         } catch (HeadlessException e) {
-            
+
             JOptionPane.showMessageDialog(null, "System Erron!\n"
-                        + "Please drop a mail to ashik.ar.420@gmail.com");
+                    + "Please drop a mail to ashik.ar.420@gmail.com");
         }
-    }    
+    }
 }
